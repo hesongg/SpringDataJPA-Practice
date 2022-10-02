62694378cd0a91b813a533a78ce2ffb992c5254a
hesung <hsjo6864@gmail.com>
2022-10-02 15:41:15 +0900

    2022.10.01 Spring Data JPA Study Project - 쿼리 메소드 기능  - 메소드 이름으로 쿼리 생성   - 순수 JPA 리포지토리와 비교하여 쿼리 메소드 기능을 이용하여 간단한 조회 조건을 수행하는 메소드 작성   - 테스트 코드 작성

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,5 +3,12 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    //find...By - ... 에는 findHelloBy 처럼 설명이 들어가기도 한다.
+    List<Member> findTop3HelloBy();
 }
