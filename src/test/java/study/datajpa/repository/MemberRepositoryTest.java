3c0278047f0e3783d33b87d5ca802ee005b77d58
hesung <hsjo6864@gmail.com>
2022-10-03 16:38:52 +0900

    2022.10.01 Spring Data JPA Study Project - 쿼리 메소드 기능  - @QueryHints, @Lock 기능 확인  - 테스트 코드 작성

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -298,4 +298,33 @@ class MemberRepositoryTest {
 
         //then
     }
+
+    @Test
+    public void queryHint() {
+        //given
+        Member member1 = memberRepository.save(new Member("member1", 10));
+
+        em.flush();
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findReadOnlyByUsername(member1.getUsername());
+        findMember.setUsername("member2");
+
+        em.flush();
+    }
+
+    @Test
+    public void lock() {
+        //given
+        Member member1 = memberRepository.save(new Member("member1", 10));
+
+        em.flush();
+        em.clear();
+
+        //when
+        List<Member> result = memberRepository.findLockByUsername(member1.getUsername());
+    }
+
+
 }
